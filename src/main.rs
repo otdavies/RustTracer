de848800e8d1f27e2c987cb0bbe9336cdd0a0382
Oliver Davies <oliver@psyfer.io>
2022-05-10 03:41:16 -0700

    Simple normal approx

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,32 @@ use vec3::Vec3;
 const IMAGE_WIDTH: usize = 512;
 
 fn ray_color(r: &Ray) -> Vec3 {
+    // Red sphere
     let center = Vec3::new(0.0, 0.0, -1.0);
-    if hit_sphere(&center, 0.5, r) {
-        return Vec3::new(1.0, 0.0, 0.0);
+    let hit = hit_sphere(&center, 0.5, r);
+    if hit > 0.0 {
+        let n: Vec3 = (r.at(hit) - center).normalized();
+        return Vec3::new(n.x + 1.0, n.y + 1.0, n.z + 1.0).scalar(0.5);
     }
+
+    // Sky color
     let unit_dir = r.direction.normalized();
     let t = 0.5 * (unit_dir.y + 1.0);
     Vec3::new(1.0, 1.0, 1.0).scalar(1.0 - t) + Vec3::new(0.5, 0.7, 1.0).scalar(t)
 }
 
-fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> f64 {
     let oc = r.origin - *center;
-    let a = r.direction.dot(&r.direction);
-    let b = 2.0 * oc.dot(&r.direction);
-    let c = oc.dot(&oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    let a = r.direction.length_squared();
+    let half_b = oc.dot(&r.direction);
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = half_b * half_b - a * c;
+
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-half_b - discriminant.sqrt()) / (a)
+    }
 }
 
 fn main() {
